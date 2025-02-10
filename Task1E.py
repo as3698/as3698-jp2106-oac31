e29f11e06f52f8a788c98aa568f9465b538b961d
Oliver Clements <oac31@cam.ac.uk>
2025-02-10 10:39:34 +0000

    removing duplicates

--- Task1E.py
+++
@@ -1,9 +0,0 @@
-from floodsystem.geo import rivers_by_station_number
-from floodsystem.stationdata import build_station_list
-
-
-from floodsystem.stationdata import build_station_list
-from floodsystem.geo import rivers_by_station_number
-rivers_by_station_number(build_station_list, 9)
-
-
