33f98ff7f3ed3df9d4e1ba9c35cd06b372d6d74d
Jaiden Patel <jp2106@cam.ac.uk>
2025-02-10 14:09:59 +0000

    df

--- List_of_stations.py
+++ List_of_stations.py
@@ -1,18 +1,19 @@
 
 def List_of_stations():
   class list_for_testing:
-    def __init__(self, name, label, coord, river, town):
+    def __init__(self, name, label, coord, river, town, typical_range):
       self.name = name
       self.label = label
       self.coord = coord
       self.river = river
       self.town = town
+      self.typical_range = typical_range
 
-  S1 = list_for_testing("s1", "label1", (1, 2), "river1", "town1")
-  S2 = list_for_testing("s2", "label2", (2, 3), "river2", "town2")
-  S3 = list_for_testing("s3", "label3", (3, 4), "river3", "town3")
-  S4 = list_for_testing("s4", "label4", (4, 5), "river4", "town4")
-  S5 = list_for_testing("s5", "label5", (5, 6), "river5", "town5")
+  S1 = list_for_testing("s1", "label1", (1, 2), "river1", "town1", (1,2))
+  S2 = list_for_testing("s2", "label2", (2, 3), "river2", "town2" (2,1))
+  S3 = list_for_testing("s3", "label3", (3, 4), "river3", "town3", None)
+  S4 = list_for_testing("s4", "label4", (4, 5), "river4", "town4", (3,4))
+  S5 = list_for_testing("s5", "label5", (5, 6), "river5", "town5", (4,5))
 
   return [S1, S2, S3, S4, S5]
 
