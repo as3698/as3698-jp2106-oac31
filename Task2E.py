1d9bd7df0493d2c8b32f195a6955ff94810b825f
Jaiden Patel <jp2106@cam.ac.uk>
2025-02-24 15:16:53 +0000

    2E Done

--- Task2E.py
+++ Task2E.py
@@ -3,6 +3,12 @@ from datetime import datetime, timedelta
 from floodsystem.stationdata import build_station_list
 from floodsystem.utils import level_history
 from floodsystem.utils import sorted_by_key, current_level
+from floodsystem.flood import stations_highest_rel_level
+from floodsystem.flood import stations_level_over_threshold
+from floodsystem.stationdata import update_water_levels
+from floodsystem.plot import plot_water_levels
+from floodsystem.station import MonitoringStation
+
 
 
 #Typical Range outputs a tuple
@@ -14,7 +20,7 @@ dates = [datetime.strptime(date + " " + time, "%Y-%m-%d %H:%M:%S%z") for date, t
 levels = [float(level) for date, time, level in data_tuples]
 print(dates)
 '''
-stations = build_station_list()
+
 '''
 data = level_history(station)
 dates = data[0]
@@ -31,18 +37,28 @@ for s in stations:
         station = s
         break
 '''
-plot_water_levels(station, dates, levels)
+
 #Station is a  monitoring station object
-
-
-for s in stations:
-    if s.name == station_name:
-        station = s
-        break
-
+number = 5
+days = 10
+n = 0
+stations = build_station_list()
+update_water_levels(stations)
 #Find list of 5 stations with highest relative water levels
-stations
+danger_stations = stations_highest_rel_level(stations, number)
+station_names = [station[0] for station in danger_stations]
+station_levels = [station[1] for station in danger_stations]
+station = None
 #Return data for each station over the last 10 days
-
+for object in station_names:
+    for s in stations:
+        if s.name == station_names[n]:
+            station = s
+            break
+    n += 1
+   
+    dates, levels = level_history(station.name, days)
+    plot_water_levels(station, dates, levels)
+    
 #Plot the data for each station
 
--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 def plot_water_levels(station, dates, levels):
     plt.plot(dates, levels)
     num_ticks = 10  # Number of x-ticks to display
@@ -6,9 +8,11 @@ def plot_water_levels(station, dates, levels):
     plt.xlabel('date')
     plt.ylabel('water level (m)')
     plt.xticks(rotation=45);
-    plt.title(station)
-    plt.axhline(y = 0.5, color = 'r', linestyle = '-')
+    plt.title(station.name)
+    low = station.typical_range[0]
+    high = station.typical_range[1]
+    plt.axhline(y = low, color = 'r', linestyle = '-')
+    plt.axhline(y = high, color = 'g', linestyle = '-')
     plt.tight_layout()
     plt.show()
 
-plot_water_levels(station, dates, levels)
