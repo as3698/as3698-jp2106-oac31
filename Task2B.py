90172cb74d5a87d8a6f624ea1612948bbda1b632
Jaiden Patel <jp2106@cam.ac.uk>
2025-02-24 11:14:42 +0000

    slight change to 2B

--- Task2B.py
+++ Task2B.py
@@ -3,7 +3,7 @@ from floodsystem.station import MonitoringStation
 from floodsystem.flood import stations_level_over_threshold 
 from floodsystem.stationdata import update_water_levels
   
-
+tol = 0.8
 stations = build_station_list()
 update_water_levels(stations)
-print(stations_level_over_threshold(stations, 0.8))
+print(stations_level_over_threshold(stations, tol))
--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -1,6 +1,6 @@
 
 from floodsystem.station import MonitoringStation
-
+'''
 def stations_level_over_threshold(stations, tol):
     output= []
     for station in stations:
@@ -10,6 +10,19 @@ def stations_level_over_threshold(stations, tol):
                 output.append((station.name, relative_level))
                 output.sort(key=lambda x: x[1], reverse=True)
     return output
+'''
+
+def stations_level_over_threshold(stations, tol):
+    output= []
+    for station in stations:
+        if station.typical_range_consistent() and station.latest_level is not None:
+            relative_level = station.relative_water_level()
+            if float(relative_level) > float(tol) and relative_level is not None:
+                output.append((station.name, relative_level))
+                output.sort(key=lambda x: x[1], reverse=True)
+    for station in output:
+        print(station)
+
 
 def stations_highest_rel_level(stations, N):
     output= []
