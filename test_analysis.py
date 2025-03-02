c4c4c2ee4cf51afcbbe562c5475e498e890d9973
Jaiden Patel <jp2106@cam.ac.uk>
2025-03-02 22:04:47 +0000

    more committing

--- test_analysis.py
+++ test_analysis.py
@@ -10,14 +10,13 @@ def test_polyfit_1():
     levels = old_dates
     test = []
     poly, d0 = polyfit(old_dates, levels, 2)
-    for i in range (1,4):
+    for i in range (0,4):
         item = ((poly))[i]
         item = round(item, 4)
         item = int(item)
         test.append(item)
     d0 = round(d0, 4)
-    assert test == [86400000000, -84878, 0] and d0 == 0.0
-    #print(test)
+    assert test == [0, 86400000000, -84878, 0] and d0 == 0.0
     
 
 def test_polyfit_2():
@@ -25,14 +24,14 @@ def test_polyfit_2():
     levels = [1,2,3,4]
     test = []
     poly, d0 = polyfit(old_dates, levels, 2)
-    for i in range (1,4):
+    for i in range (0,4):
         item = ((poly))[i]
         item = round(item, 3)
         item = int(item)
         test.append(item)
     d0 = round(d0, 4)
     #print(test)
-    assert test == [86400000000, 1964169, 0] and d0 == 0.0
+    assert test == [0, 86400000000, 1964169, 0] and d0 == 0.0
 
 
 
